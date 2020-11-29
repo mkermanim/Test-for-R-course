c7c32b959e4de4368c72b8540c8c55a91307bfb3
Hossein Kermani <spm.kermani@yahoo.com>
2020-11-29 10:44:48 +0330

    Commit new 3 files

--- WEEK2.R
+++ WEEK2.R
@@ -1,1 +1,12 @@
 # R Course Practice- Week 2
+
+# Installing packages
+
+install.packages("tidyverse")
+library(tidyverse)
+library(dplyr)
+
+# Calling data
+install.packages("gapminder")
+library(gapminder)
+
