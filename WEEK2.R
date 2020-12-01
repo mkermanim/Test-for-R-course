77cbd504de256c7cc07124408eab2ebb74db9b4a
Hossein Kermani <spm.kermani@yahoo.com>
2020-12-01 10:52:01 +0330

    commint some works on ggplot

--- WEEK2.R
+++ WEEK2.R
@@ -4,9 +4,19 @@
 
 install.packages("tidyverse")
 library(tidyverse)
+install.packages("dplyr")
 library(dplyr)
+library(ggplot2)
 
 # Calling data
 install.packages("gapminder")
 library(gapminder)
+str(gapminder) #Looking at data
+China <- gapminder %>% filter(country=="China") # Filtering China data
+plot(data=China, 
+     lifeExp ~ year,
+     pch=12) # Using basic plot 
 
+ggplot(data = China, aes(x=year,y=lifeExp))+
+  geom_point(color="red", pch=12, alpha=0.7)+
+  theme_minimal() #Using ggplot
