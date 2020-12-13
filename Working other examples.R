98dc8b303c1c876005986239bd072661d77b06a7
Hossein Kermani <spm.kermani@yahoo.com>
2020-12-13 18:18:27 +0330

    new

---
+++ Working other examples.R
@@ -0,0 +1,28 @@
+#Working other examples
+library(tibble)
+library(dplyr)
+library(gapminder)
+df <- tibble(
+  x = sample(10, 100, rep = TRUE),
+  y = sample(10, 100, rep = TRUE)
+)
+nrow(df)
+nrow(distinct(df))
+nrow(distinct(df, x, y))
+
+lm_pop_year <- gapminder %>%
+  filter(continent == "Americas") %>%
+  lm(pop ~ year, data = .)
+lm_pop_year
+
+
+former_yugoslavia <- c("Bosnia and Herzegovina", "Croatia",
+                       "Montenegro", "Serbia", "Slovenia")
+yugoslavia <- gapminder %>% filter(country %in% former_yugoslavia) 
+tail(yugoslavia, 2)
+
+set.seed(500)
+yugoslavia %>% sample_n(size = 6, replace = FALSE)
+
+example <- c(1, 0, "asdg", -2)
+ifelse(example > 0, "Positive", "Not Positive")
