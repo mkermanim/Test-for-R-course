e54ebdae151bfcc63e1e9c3368f4767580477b1c
Hossein Kermani <spm.kermani@yahoo.com>
2020-12-15 11:19:56 +0330

    New on dplyr

--- Working other examples.R
+++ Working other examples.R
@@ -2,6 +2,10 @@
 library(tibble)
 library(dplyr)
 library(gapminder)
+
+
+
+
 df <- tibble(
   x = sample(10, 100, rep = TRUE),
   y = sample(10, 100, rep = TRUE)
@@ -26,3 +30,11 @@ yugoslavia %>% sample_n(size = 6, replace = FALSE)
 
 example <- c(1, 0, "asdg", -2)
 ifelse(example > 0, "Positive", "Not Positive")
+
+yugoslavia %>% arrange(desc(year), pop)
+
+
+yugoslavia %>%
+  filter(year == 1982) %>%
+  summarize()
+            
