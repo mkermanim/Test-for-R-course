b78728c7a4962530a25f1f1ed98785628ae190ce
Hossein Kermani <spm.kermani@yahoo.com>
2020-12-25 10:57:13 +0330

    25 Dec practices

--- Merging Dframes.R
+++ Merging Dframes.R
@@ -2,5 +2,10 @@
 install.packages("nycflights13")
 library(nycflights13)
 
-flights %>% left_join(planes)
-View(planes)
+fp <- flights %>% filter(arr_time==830) %>%  left_join(planes %>% select(model), by="tailnum")
+View(fp)
+
+flights %>% filter(dest == "SEA") %>% select(tailnum) %>%
+  left_join(planes %>% select(tailnum, manufacturer)) %>%
+  count(manufacturer) %>% # Count observations by manufacturer
+  arrange(desc(n))
