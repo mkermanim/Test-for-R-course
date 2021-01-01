67490770471bf4b8832dc0a0bfc1073476e6c167
Hossein Kermani <spm.kermani@yahoo.com>
2021-01-01 19:50:02 +0330

    new update

--- TidyData.R
+++ TidyData.R
@@ -1,3 +1,4 @@
+install.packages("tidyr")
 library(tidyr)
 
 billboard_2000_raw <- read_csv(file = "https://clanfear.github.io/CSSS508/Lectures/Week5/data/billboard.csv")
@@ -12,15 +13,23 @@ billboard_2000_raw <-
 billboard_2000 <- billboard_2000_raw %>%
   pivot_longer (starts_with("wk"),
                names_to ="week",
-               values_to = "rank")
+               values_to = "rank",
+               values_drop_na = TRUE,
+               )
 
-billboard_2000 <- billboard_2000_raw %>% 
+
+billboard_2000 <- billboard_2000 %>% 
+  mutate(week=parse_number(week))
+
+billboard_2000 <- billboard_2000 %>% 
   separate(time, into = c("min", "sec"),
            sep = ":", convert = TRUE) %>% 
   mutate(lenght= min + sec/60) %>% 
   select(-min,-sec)
 
   summary(billboard_2000$lenght)
+  
+  
+  
 
-
-pivot_wider()
+  
