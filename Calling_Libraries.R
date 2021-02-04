83f09bb4d7865eda67ac33241c9ee064a638cf2c
Hossein Kermani <spm.kermani@yahoo.com>
2021-02-04 10:48:05 +0330

    Feb 4 excercise

--- Calling_Libraries.R
+++ Calling_Libraries.R
@@ -12,3 +12,9 @@ library(lubridate)
 library(stringr)
 library(nycflights13)
 library(haven)
+
+#For the case I wanted to install these packages
+#This solotion is taken from https://socviz.co/ 
+libs <- c("tibble","dplyr", "gapminder","tidyverse","pander","xlsx","readr","tidyr","ggplot2",
+          "lubridate","stringr","nycflights13","haven")
+install.packages(libs, repos = "http://cran.rstudio.com") 
--- Working with dplyr.R
+++ Working with dplyr.R
@@ -2,17 +2,4 @@
 # reading dataset 
 tel_test_data<-read_sav("C:/Users/Hossein/Documents/MEGA/Papers/Completed Works/ML Project/Social Media Lit in Telegram/Data/ML Data.sav")
 
-tel_test_data<- tel_test_data(na.rm=TRUE)
-inc_city<- tel_test_data %>% group_by(City) %>% 
-    summarize(incmean=mean(Income, na.rm=TRUE), N=n()) #%>% pander()
-inc_city %>% rename_all (recode, incmean="MeanIncome") %>% arrange(desc(MeanIncome),desc(N))
-inc_city %>%  ggplot(aes(x=incmean, y=N))+ 
-  geom_point(pch=10, color="red")+theme_classic()
 
-(tel_test_data %>% transmute(test=mean(EngagementinGs_1)+var(FakeNewsReaction2_1)))
-mean(!is.na(tel_test_data$EngagementinGs_1))
-
-a_2_var<-tel_test_data %>% group_by(Gender_1) %>% summarise(sdd=sd(EngagementinGs_1),
-                                                   minn=min(FakeNewsKnowledge_1),
-                                                   N=n())
-a_2_var %>% ggplot(aes(sdd,minn), size=N)+geom_point()
